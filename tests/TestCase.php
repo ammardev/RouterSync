2f4a87e2ce6d6f04cefa122ab37419251a9efb3e
Ammar Al-Khawaldeh <me@ammar.dev>
2020-02-15 12:42:57 +0200

    Initialize testing for the package

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace Luqta\RouterSync\Test;
+
+use Luqta\RouterSync\Facades\RouterSync;
+use Luqta\RouterSync\RouterSyncServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return lasselehtinen\MyPackage\MyPackageServiceProvider
+     */
+    protected function getPackageProviders($app)
+    {
+        return [RouterSyncServiceProvider::class];
+    }
+    /**
+     * Load package alias
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'RouterSync' => RouterSync::class,
+        ];
+    }
+}
