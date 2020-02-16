6656b08af1dc670c933e31377c97d65fb9ee290e
Ammar Al-Khawaldeh <me@ammar.dev>
2020-02-16 14:55:42 +0200

    Add urls attributes to the request

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,6 +4,8 @@ namespace Luqta\RouterSync\Http;
 
 class Request
 {
+    public $original_url;
+    protected $matched_url;
     protected $headers;
     protected $body;
     protected $files;
@@ -26,5 +28,17 @@ class Request
 
     public function getBody(): array {
         return $this->body;
+    }
+
+    public function setMatchedUrl($parameters)
+    {
+        $this->matched_url = $this->replaceRouteParameters($this->original_url, $parameters);
+    }
+
+    protected function replaceRouteParameters($route, &$parameters = [])
+    {
+        return preg_replace_callback('/\{(.*?)(:.*?)?(\{[0-9,]+\})?\}/', function ($m) use (&$parameters) {
+            return isset($parameters[$m[1]]) ? Arr::pull($parameters, $m[1]) : $m[0];
+        }, $route);
     }
 }
