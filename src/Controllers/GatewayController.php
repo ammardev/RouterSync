371056179f87db8bf9771dd78335d5f3c5d62672
Ammar Al-Khawaldeh <me@ammar.dev>
2020-02-16 15:08:55 +0200

    Use the resolver in the controller

--- src/Controllers/GatewayController.php
+++ src/Controllers/GatewayController.php
@@ -5,50 +5,29 @@ namespace Luqta\RouterSync\Controllers;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
+use Luqta\RouterSync\Http\IlluminateRequestResolver;
 
 class GatewayController extends Controller
 {
     protected $http;
+    protected $resolver;
 
-    public function __construct(Client $http)
+    public function __construct(Client $http, IlluminateRequestResolver $resolver)
     {
         $this->http = $http;
+        $this->resolver = $resolver;
     }
 
-    protected function replaceRouteParameters($route, &$parameters = [])
+    public function requestMicroservice(Request $illuminateRequest)
     {
-        return preg_replace_callback('/\{(.*?)(:.*?)?(\{[0-9,]+\})?\}/', function ($m) use (&$parameters) {
-            return isset($parameters[$m[1]]) ? Arr::pull($parameters, $m[1]) : $m[0];
-        }, $route);
-    }
+        $this->resolver->setRequest($illuminateRequest);
+        $request = $this->resolver->resolve();
 
-    public function requestMicroservice(Request $request)
-    {
-        $original_uri = $request->route()[1]['original_uri'];
-        $matchedUrl = $this->replaceRouteParameters($original_uri, $request->route()[2]);
-
-        $headers = [
-                'Accept' => 'application/json',
-        ];
-        if ($request->header('Authorization')) {
-                $headers['Authorization'] = $request->header('Authorization');
-        }
-
-        $files = $request->allFiles();
-        $multipart = [];
-        foreach ($files as $key => $file) {
-            $multipart[] = [
-                'name' => $key,
-                'contents' => fopen($file->getRealPath(), 'r'),
-                'fileName' => $file->getClientOriginalName(),
-            ];
-        }
-        $response = $this->http->request($request->method(), config('app.url') . '/' . $matchedUrl, [
-            'http_errors' => false,
-            'form_params' => $request->all(),
-            'headers' => $headers
-        ]);
+        $response = $this->http->request(
+            $request->method,
+            config('app.url') . '/' . $request->matched_url,
+            $request->toArray()
+        );
 
         return response($response->getBody(), $response->getStatusCode());
     }
